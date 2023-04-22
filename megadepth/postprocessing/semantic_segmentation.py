38604eab1090db830ea4082f6c531dc6483832d0
Felix Yang <79040988+fyangch@users.noreply.github.com>
2023-04-22 10:00:40 +0200

    Automatically download model weights, change plots

--- megadepth/postprocessing/semantic_segmentation.py
+++ megadepth/postprocessing/semantic_segmentation.py
@@ -1,9 +1,19 @@
 """Functions that implement the semantic segmentation steps for the cleanup.."""
+import os
+from urllib.request import urlretrieve
+
 import numpy as np
 import torch
 from mit_semseg.models import ModelBuilder, SegmentationModule
 from PIL import Image
 from torchvision import transforms
+
+encoder_url = (
+    "http://sceneparsing.csail.mit.edu/model/pytorch/ade20k-hrnetv2-c1/encoder_epoch_30.pth"
+)
+decoder_url = (
+    "http://sceneparsing.csail.mit.edu/model/pytorch/ade20k-hrnetv2-c1/decoder_epoch_30.pth"
+)
 
 pil_to_tensor = transforms.Compose(
     [
@@ -24,13 +34,16 @@ def get_segmentation_model() -> SegmentationModule:
     Returns:
         SegmentationModule: The HRNetV2 segmentation model.
     """
-    # TODO: update the weights path
-    encoder = ModelBuilder.build_encoder(
-        "hrnetv2", fc_dim=720, weights="./pretrained/hrnetv2/encoder_epoch_30.pth"
-    )
-    decoder = ModelBuilder.build_decoder(
-        "c1", fc_dim=720, use_softmax=True, weights="./pretrained/hrnetv2/decoder_epoch_30.pth"
-    )
+    # download pre-trained weights if necessary
+    encoder_path = os.path.join("pretrained", "hrnetv2", "encoder_epoch_30.pth")
+    decoder_path = os.path.join("pretrained", "hrnetv2", "decoder_epoch_30.pth")
+    if not os.path.exists(encoder_path) or not os.path.exists(decoder_path):
+        os.makedirs(os.path.join("pretrained", "hrnetv2"), exist_ok=True)
+        urlretrieve(encoder_url, encoder_path)
+        urlretrieve(decoder_url, decoder_path)
+
+    encoder = ModelBuilder.build_encoder("hrnetv2", fc_dim=720, weights=encoder_path)
+    decoder = ModelBuilder.build_decoder("c1", fc_dim=720, use_softmax=True, weights=decoder_path)
 
     crit = torch.nn.NLLLoss(ignore_index=-1)
     model = SegmentationModule(encoder, decoder, crit)
