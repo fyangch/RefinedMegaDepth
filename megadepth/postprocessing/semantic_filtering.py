0199bf48373ecb34137f54d83c3e49b6ff744279
Felix Yang <79040988+fyangch@users.noreply.github.com>
2023-04-22 11:02:14 +0200

    Extract foreground/background/sky masks

--- megadepth/postprocessing/semantic_filtering.py
+++ megadepth/postprocessing/semantic_filtering.py
@@ -1,5 +1,62 @@
 """Functions that implement the semantic filtering steps for the cleanup.."""
+from typing import Literal
+
 import numpy as np
+
+foreground_labels = np.array(
+    [
+        12,
+        15,
+        19,
+        31,
+        43,
+        66,
+        67,
+        69,
+        76,
+        80,
+        83,
+        87,
+        88,
+        100,
+        102,
+        103,
+        104,
+        115,
+        116,
+        119,
+        126,
+        127,
+        132,
+        136,
+        144,
+    ]
+)
+background_labels = np.array([1, 25, 48, 68, 84, 113, 16])
+
+
+def get_mask(
+    segmentation_map: np.ndarray, mask_type: Literal["foreground", "background", "sky"]
+) -> np.ndarray:
+    """Return the foreground/background/sky mask given a segmentation mask.
+
+    Args:
+        segmentation_map (np.ndarray): Predicted segmentation map.
+        mask_type (Literal["foreground", "background", "sky"]): Mask type.
+
+    Returns:
+        np.ndarray: Mask with the same shape as the segmentation map.
+    """
+    if mask_type == "foreground":
+        mask = np.in1d(segmentation_map, foreground_labels)
+        return np.reshape(mask, segmentation_map.shape)
+    elif mask_type == "background":
+        mask = np.in1d(segmentation_map, background_labels)
+        return np.reshape(mask, segmentation_map.shape)
+    elif mask_type == "sky":
+        return segmentation_map == 2
+    else:
+        raise ValueError(f"Invalid mask type: {mask_type}")
 
 
 # TODO: Change function signature if necessary
