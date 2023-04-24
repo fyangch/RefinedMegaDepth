7fee70d5ff78a6ef891aca5c29df0e5395ff42a0
Andri <horat.andri@bluewin.ch>
2023-04-24 11:14:20 +0200

    implemented median filter and check_semantics

--- megadepth/postprocessing/image_processing.py
+++ megadepth/postprocessing/image_processing.py
@@ -2,6 +2,31 @@
 import numpy as np
 
 
+def median_filter(img, kernel_size=3):
+    """Median filter that ignores zeros and nans."""
+    # Replace zeros with NaNs
+    img_nan = np.where(img == 0, np.nan, img)
+
+    # Pad the image with NaNs
+    img_padded = np.pad(img_nan, 2, mode="constant", constant_values=np.nan)
+
+    # Apply median filter
+    filtered_img = np.zeros_like(img)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            # Extract the subarray centered at (i, j)
+            subarray = img_padded[:5, :5]  # ,img_padded[i : i + kernel_size, j : j + kernel_size]
+            # Ignore NaNs and zeros in the subarray
+            subarray = subarray[np.logical_and(~np.isnan(subarray), subarray != 0)]
+            # Calculate the median of the subarray
+            if subarray.size > 0:
+                filtered_img[i, j] = np.nanmedian(subarray)
+            else:
+                filtered_img[i, j] = 0
+
+    return filtered_img
+
+
 # TODO: Change function signature if necessary
 def filter_unstable_depths(
     depth_map: np.ndarray,
@@ -20,14 +45,21 @@ def filter_unstable_depths(
     """
     # TODO: Supplementary material, Algorithm 1, line 10-11
     # 1. apply 5x5 median filter
+    median_filtered = median_filter(depth_map, kernel_size)
+    # avoid division by zero, max(D/Df, Df/D)> thresh
+    unstable1 = median_filtered > threshold * depth_map
+    unstable2 = depth_map > threshold * median_filtered
+    # a pixel is stable unless one of the criterions makes it unstable
+    stable = 1 - unstable1 * unstable2
     # 2. remove "unstable pixels"
-
-    raise NotImplementedError()
+    # filtered = cv2.bitwise_and(depth_map, depth_map, mask=stable)
+    filtered = depth_map * stable
+    return filtered
 
 
 # TODO: Change function signature if necessary
-def erode_and_remove(depth_map: np.ndarray) -> np.ndarray:
-    """Erode the depth map and remove the eroded pixels.
+def erode_and_remove(depth_map: np.ndarray, segmentation_map: np.ndarray) -> np.ndarray:
+    """Erode the segmentation map and remove the small connected components.
 
     Args:
         depth_map (np.ndarray): Depth map.
@@ -36,5 +68,14 @@ def erode_and_remove(depth_map: np.ndarray) -> np.ndarray:
         np.ndarray: Eroded depth map and filtered depth map.
     """
     # TODO: Supplementary material, Algorithm 1, line 18
+
+    # 1. compute connected components from foreground segmentation
+
+    # 2. for each connected component
+    # if C is small: continue
+
+    # a. count percentage of valid depths
+
+    # if percentage is < 50% mask out the component
 
     raise NotImplementedError()
--- megadepth/postprocessing/semantic_filtering.py
+++ megadepth/postprocessing/semantic_filtering.py
@@ -2,6 +2,7 @@
 from typing import Literal
 
 import numpy as np
+from skimage.measure import label
 
 foreground_labels = np.array(
     [
@@ -59,6 +60,28 @@ def get_mask(
         raise ValueError(f"Invalid mask type: {mask_type}")
 
 
+def check_semantics(mask, depth_map, threshold=0.5):
+    """Check percentage of valid depths per segmentation."""
+    # Find connected components in mask
+    labeled_mask, num_features = label(mask, background=0, connectivity=2, return_num=True)
+
+    # Iterate over each connected component
+    for i in range(1, num_features + 1):
+        # Get depth values for current component
+        depth_vector = depth_map[labeled_mask == i]
+
+        # Compute ratio of NaN values to connected pixels
+        num_pixels_connected = np.sum(labeled_mask == i)
+
+        ratio = np.sum(np.count_nonzero(depth_vector)) / num_pixels_connected
+        print(ratio)
+        # If ratio is smaller than 0.5, set depth values to NaN/ 0
+        if ratio < threshold:
+            depth_map[labeled_mask == i] = 0
+
+    return depth_map
+
+
 # TODO: Change function signature if necessary
 def apply_semantic_filtering(
     depth_map: np.ndarray,
@@ -81,12 +104,24 @@ def apply_semantic_filtering(
     # background_mask = get_mask(segmentation_map, "background")
     # sky_mask = get_mask(segmentation_map, "sky")
 
+    # 1. compute connected components from foreground segmentation
+
+    # 2. for each connected component
+    # if C is small: continue
+
+    # a. count percentage of valid depths
+
+    # if percentage is < 50% mask out the component
+
     raise NotImplementedError()
 
 
 # TODO: Change function signature if necessary
-def is_selfie_image(depth_map: np.ndarray, segmentation_map: np.ndarray) -> bool:
+def is_selfie_image(depth_map: np.ndarray, segmentation_map: np.ndarray, threshold=0.35) -> bool:
     """Check if the image is a selfie image.
+
+    Returns True if depth coverage is bigger than threshold.
+    for scenes [168, 229, 212, 768] they use 0.2 as threshold.
 
     Args:
         depth_map (np.ndarray): The depth map.
@@ -96,8 +131,11 @@ def is_selfie_image(depth_map: np.ndarray, segmentation_map: np.ndarray) -> bool
         bool: True if the image is a selfie image, False otherwise.
     """
     # I guess this is the very last step? (not shown in Algorithm 1 of the supplements...)
-
-    raise NotImplementedError()
+    # ignore sky
+    sky_mask = get_mask(segmentation_map, "sky")
+    num_valid = np.count_nonzero(sky_mask != 0)
+    num_valid_depth = np.count_nonzero(depth_map)
+    return num_valid > threshold * num_valid_depth
 
 
 # not sure yet about this step
