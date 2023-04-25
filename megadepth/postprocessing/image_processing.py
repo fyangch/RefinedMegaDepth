c177df127b9ac201acdaa069cb0ccfad07b8ea01
Felix Yang <79040988+fyangch@users.noreply.github.com>
2023-04-25 12:47:09 +0200

    Fix boolean logic, use median filter from opencv

--- megadepth/postprocessing/image_processing.py
+++ megadepth/postprocessing/image_processing.py
@@ -1,63 +1,40 @@
 """Functions that perform image processing steps required for the cleanup."""
+import cv2
 import numpy as np
 
 
-def median_filter(img, kernel_size=3):
-    """Median filter that ignores zeros and nans."""
-    # Replace zeros with NaNs
-    img_nan = np.where(img == 0, np.nan, img)
-
-    # Pad the image with NaNs
-    img_padded = np.pad(img_nan, 2, mode="constant", constant_values=np.nan)
-
-    # Apply median filter
-    filtered_img = np.zeros_like(img)
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            # Extract the subarray centered at (i, j)
-            subarray = img_padded[:5, :5]  # ,img_padded[i : i + kernel_size, j : j + kernel_size]
-            # Ignore NaNs and zeros in the subarray
-            subarray = subarray[np.logical_and(~np.isnan(subarray), subarray != 0)]
-            # Calculate the median of the subarray
-            if subarray.size > 0:
-                filtered_img[i, j] = np.nanmedian(subarray)
-            else:
-                filtered_img[i, j] = 0
-
-    return filtered_img
-
-
-# TODO: Change function signature if necessary
 def filter_unstable_depths(
     depth_map: np.ndarray,
     kernel_size: int = 5,
     threshold: float = 1.15,
 ) -> np.ndarray:
-    """Filter unstable depths by removing unstable pixels.
+    """Apply median filter and keep depth values that are close enough to the corresponding median.
 
     Args:
-        depth_map (np.ndarray): _description_
+        depth_map (np.ndarray): Depth map.
         kernel_size (int, optional): Size of the median filter kernel. Defaults to 5.
-        threshold (float, optional): Threshold for the depth difference. Defaults to 1.15.
+        threshold (float, optional): Threshold for the relative depth difference. Defaults to 1.15.
 
     Returns:
         np.ndarray: Filtered depth map.
     """
-    # TODO: Supplementary material, Algorithm 1, line 10-11
-    # 1. apply 5x5 median filter
-    median_filtered = median_filter(depth_map, kernel_size)
-    # avoid division by zero, max(D/Df, Df/D)> thresh
-    unstable1 = median_filtered > threshold * depth_map
-    unstable2 = depth_map > threshold * median_filtered
-    # a pixel is stable unless one of the criterions makes it unstable
-    stable = 1 - unstable1 * unstable2
-    # 2. remove "unstable pixels"
-    # filtered = cv2.bitwise_and(depth_map, depth_map, mask=stable)
-    filtered = depth_map * stable
-    return filtered
+    # apply median filter
+    median_filtered = cv2.medianBlur(depth_map, kernel_size)
+
+    # a depth value D is unstable if max(D/M, M/D) > threshold where M is the corresponding median
+    unstable_1 = median_filtered > threshold * depth_map  # avoid division by 0....
+    unstable_2 = depth_map > threshold * median_filtered
+    unstable = unstable_1 | unstable_2
+
+    # only consider removing valid depth values from the original depth map
+    valid = depth_map > 0.0
+    unstable = unstable & valid
+
+    # remove unstable pixels from the median filtered depth map
+    stable = np.logical_not(unstable)
+    return median_filtered * stable
 
 
-# TODO: Change function signature if necessary
 def erode_and_remove(depth_map: np.ndarray, segmentation_map: np.ndarray) -> np.ndarray:
     """Erode the segmentation map and remove the small connected components.
 
@@ -68,14 +45,5 @@ def erode_and_remove(depth_map: np.ndarray, segmentation_map: np.ndarray) -> np.
         np.ndarray: Eroded depth map and filtered depth map.
     """
     # TODO: Supplementary material, Algorithm 1, line 18
-
-    # 1. compute connected components from foreground segmentation
-
-    # 2. for each connected component
-    # if C is small: continue
-
-    # a. count percentage of valid depths
-
-    # if percentage is < 50% mask out the component
 
     raise NotImplementedError()
