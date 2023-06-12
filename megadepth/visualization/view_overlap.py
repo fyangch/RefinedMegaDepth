9060090f93d8a6def2e1019e2f6b16dbae3b4e93
Andri <horat.andri@bluewin.ch>
2023-06-12 19:05:43 +0200

    do correct alpha blending

--- megadepth/visualization/view_overlap.py
+++ megadepth/visualization/view_overlap.py
@@ -25,7 +25,9 @@ def show_matrix(matrix: np.ndarray, path: Optional[str] = None):
     plt.close(fig)
 
 
-def vis_overlap(matrix, poses, points=np.zeros((0, 3)), path=None, *args, **kwargs):
+def vis_overlap(
+    matrix, poses, points=np.zeros((0, 3)), opacity=0.1, path=None, color="orange", *args, **kwargs
+):
     """Shows overlap score as opacity on lines between camera poses.
 
     For performance all overlaps smaller than 0.1 are thrown away.
@@ -35,18 +37,20 @@ def vis_overlap(matrix, poses, points=np.zeros((0, 3)), path=None, *args, **kwar
         matrix: Overlap matrix NxN np.ndarray
         poses: Nx3 np.ndarray
         points: (optional)
+        opacity: opacity value that is used for a score of 1
         path: filepath to store the plot
+        color: line colors
     """
     fig = plt.figure(figsize=(10, 10))
     plt.tight_layout()
 
     # Calculate coordinates and weights of all pairs of cameras
     i, j = np.triu_indices(len(poses), k=1)
-
-    weights = (matrix + matrix.T)[i, j].astype(float) / 10
+    a, b = matrix[i, j], matrix[j, i]
+    weights = (a + (1 - a) * b).astype(float) * opacity
 
     # sorted = np.argsort(-weights)[:10000]
-    sorted = weights > 0.01
+    sorted = weights > 0.00
     i = i[sorted]
     j = j[sorted]
     weights = weights[sorted]
@@ -55,7 +59,7 @@ def vis_overlap(matrix, poses, points=np.zeros((0, 3)), path=None, *args, **kwar
     y = np.stack((poses[i, 1], poses[j, 1]), axis=-1)
 
     # Create LineCollection object with coordinates and weights
-    lines = LineCollection(np.stack([x, y], axis=-1), linewidths=1, colors="orange", alpha=weights)
+    lines = LineCollection(np.stack([x, y], axis=-1), linewidths=1, colors=color, alpha=weights)
 
     # Add LineCollection to plot
     ax = plt.gca()
